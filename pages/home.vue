f12721f74d9d6c9fbc5f93494fa8efd32a8512e6
rendiramdani <mochfaizar45@gmail.com>
2022-02-17 07:37:30 +0700

    initial commit

---
+++ pages/home.vue
@@ -0,0 +1,12 @@
+<template>
+    <main>
+       <div class="container">
+        <Navigasi />
+          <div class="row">
+            <div class="col-md-12">
+              <h1>Products</h1>
+            </div>
+        </div>
+    </div>
+  </main>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+   <main>
+      <div class="container">
+        <Navigasi />
+          <div class="row">
+            <div class="col-md-12">
+              <h1>Products</h1>
+            </div>
+          </div>
+        </div>
+    </main>
+</template>
+
+<script>
+export default {
+  mounted(){
+    console.log(this.$supabase)
+  }
+}
+</script>
